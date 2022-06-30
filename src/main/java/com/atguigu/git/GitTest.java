fdad2ffafe8aab7540c8e1feccabee14a67f9cb8
test <1515616516589@qq.com>
2022-06-30 19:30:35 +0800

    hot-fix-3

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -6,5 +6,6 @@ public class GitTest {
         System.out.println("hello git2");
         System.out.println("hello git3 hot-fix1");
         System.out.println("hello git4 hot-fix2-test");
+        System.out.println("hello git5 hot-fix3-test");
     }
 }
